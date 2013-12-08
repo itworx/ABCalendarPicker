012474f0afe29d2f2298048c866d34eea09538b7
Mourad-Aly <Mourad@Mourads-MacBook-Pro.local>
2013-12-08 13:18:29 +0200

    TeacherKit Provider n Updates

--- Demo/ABCalendarPickerDemo/ABViewController.m
+++ Demo/ABCalendarPickerDemo/ABViewController.m
@@ -10,115 +10,61 @@
 #import <ABCalendarPicker/ABCalendarPicker.h>
 #import "ABViewController.h"
 
-@interface ABViewController () <UITableViewDelegate,UITableViewDataSource,ABCalendarPickerDelegateProtocol,ABCalendarPickerDataSourceProtocol>
-@property (assign, nonatomic) IBOutlet ABCalendarPicker *calendarPicker;
-@property (strong, nonatomic) UIImageView * calendarShadow;
-@property (unsafe_unretained, nonatomic) IBOutlet UITableView *eventsTable;
-@property (unsafe_unretained, nonatomic) IBOutlet UIView *configPanel;
+@interface ABCalendarPickerDefaultTeacherKitDaysProvider : ABCalendarPickerDefaultDaysProvider
 
-@property (nonatomic) EKEventStore * store;
+@end
+
+
+@implementation ABCalendarPickerDefaultTeacherKitDaysProvider
+- (NSString*)labelForDate:(NSDate*)date
+{
+    NSUInteger day = [self.calendar ordinalityOfUnit:NSDayCalendarUnit inUnit:NSMonthCalendarUnit forDate:date];
+    return [NSString stringWithFormat:@"%@\n%i", [self columnName:day], day, nil];
+}
+-(NSDate*)dateForLongPrevAnimation
+{
+    return nil;
+}
+-(NSDate*)dateForLongNextAnimation
+{
+    return nil;
+}
+@end
+
+@interface ABCalendarPickerDefaultTeacherKitWeekDaysProvider : ABCalendarPickerDefaultWeekdaysProvider
+
+@end
+
+
+@implementation ABCalendarPickerDefaultTeacherKitWeekDaysProvider
+- (NSString*)labelForDate:(NSDate*)date
+{
+    NSUInteger day = [self.calendar ordinalityOfUnit:NSDayCalendarUnit inUnit:NSMonthCalendarUnit forDate:date];
+    return [NSString stringWithFormat:@"%@\n%i", [self columnName:day], day, nil];
+}
+-(NSDate*)dateForLongPrevAnimation
+{
+    return nil;
+}
+-(NSDate*)dateForLongNextAnimation
+{
+    return nil;
+}
+@end
+
+@interface ABViewController () <ABCalendarPickerDelegateProtocol,ABCalendarPickerDataSourceProtocol>
+@property (assign, nonatomic) IBOutlet ABCalendarPicker *calendarPicker;
 @end
 
 @implementation ABViewController
 
-- (EKEventStore *)store
-{
-    if (_store == nil)
-    {
-        _store = [[EKEventStore alloc] init];
-        if ([[EKEventStore class] resolveClassMethod:@selector(authorizationStatusForEntityType:)])
-        {
-            if ([EKEventStore authorizationStatusForEntityType:(EKEntityTypeEvent)] != EKAuthorizationStatusAuthorized)
-                [_store requestAccessToEntityType:(EKEntityTypeEvent) completion:^(BOOL granted, NSError *error) {
-                    ;
-                }];
-        }
-    }
-    return _store;
-}
-
-- (NSArray *)eventsForDate:(NSDate *)date
-{
-    NSDateComponents * componentsBegin = [self.calendarPicker.calendar components:NSYearCalendarUnit|NSMonthCalendarUnit|NSDayCalendarUnit fromDate:date];
-    NSDateComponents * componentsDay = [[NSDateComponents alloc] init];
-    componentsDay.day = 1;
-    
-    NSDate * dayBegin = [self.calendarPicker.calendar dateFromComponents:componentsBegin];
-    NSDate * dayEnd = [self.calendarPicker.calendar dateByAddingComponents:componentsDay toDate:dayBegin options:0];
-    
-    NSPredicate * predicate = [self.store predicateForEventsWithStartDate:dayBegin endDate:dayEnd calendars:nil];
-    return [self.store eventsMatchingPredicate:predicate];
-}
-
-- (UIImageView*)calendarShadow
-{
-    if (_calendarShadow == nil)
-    {
-        _calendarShadow = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"CalendarShadow"]];
-        _calendarShadow.opaque = NO;
-    }
-    return _calendarShadow;
-}
-
-- (IBAction)configTapped:(id)sender
-{
-    self.view.userInteractionEnabled = NO;
-    [UIView animateWithDuration:0.3
-                          delay:0.0
-                        options:(UIViewAnimationOptionCurveEaseInOut)
-                     animations:^{
-                         CGFloat y = self.view.bounds.size.height;
-                         CGFloat delta = self.configPanel.bounds.size.height / 2;
-                         if (self.configPanel.center.y < self.view.bounds.size.height)
-                             y += delta;
-                         else
-                             y -= delta;
-                         self.configPanel.center = CGPointMake(self.configPanel.center.x, y);
-                     } completion:^(BOOL finished) {
-                         self.view.userInteractionEnabled = YES;
-                     }];
-}
 
 - (IBAction)todayTapped:(id)sender
 {
     [self.calendarPicker setDate:[NSDate date] andState:ABCalendarPickerStateDays animated:YES];
 }
 
-- (IBAction)monthGridChange:(UISegmentedControl *)sender
-{
-    if (sender.selectedSegmentIndex == 0)
-        [self.calendarPicker setMonthsProvider:[[ABCalendarPickerDefaultMonthsProvider alloc] init]];
-    else
-        [self.calendarPicker setMonthsProvider:[[ABCalendarPickerDefaultSeasonedMonthsProvider alloc] init]];
-}
-
-- (IBAction)firstWeekdayChange:(UISegmentedControl *)sender
-{
-    self.calendarPicker.calendar.firstWeekday = (sender.selectedSegmentIndex + 1)%7 + 1;
-    [self.calendarPicker updateStateAnimated:YES];
-}
-
-- (IBAction)threeWeekChanged:(UISegmentedControl *)sender
-{
-    if (sender.selectedSegmentIndex == 0)
-        self.calendarPicker.weekdaysProvider = [[ABCalendarPickerDefaultWeekdaysProvider alloc] init];
-    else
-        self.calendarPicker.weekdaysProvider = [[ABCalendarPickerDefaultTripleWeekdaysProvider alloc] init];
-    [self.calendarPicker updateStateAnimated:YES];
-}
-
 #pragma mark - ABCalendarPicker delegate and dataSource
-
-- (void)calendarPicker:(ABCalendarPicker *)calendarPicker
-      animateNewHeight:(CGFloat)height
-{
-    self.calendarShadow.frame = CGRectMake(0,CGRectGetMaxY(self.calendarPicker.frame),
-                                           self.calendarPicker.frame.size.width,
-                                           self.calendarShadow.frame.size.height);
-    self.eventsTable.frame = CGRectMake(0, CGRectGetMaxY(self.calendarPicker.frame),
-                                        self.eventsTable.bounds.size.width,
-                                        self.view.bounds.size.height - self.calendarPicker.bounds.size.height);
-}
 
 - (NSInteger)calendarPicker:(ABCalendarPicker*)calendarPicker
       numberOfEventsForDate:(NSDate*)date
@@ -130,39 +76,14 @@
         return 0;
     }
     
-    return [[self eventsForDate:date] count];
+    return 0;
 }
 
 - (void)calendarPicker:(ABCalendarPicker *)calendarPicker
           dateSelected:(NSDate *)date
              withState:(ABCalendarPickerState)state
 {
-    [self.eventsTable reloadData];
-}
-
-#pragma mark - UITableView delegate and dataSource
-
-- (NSInteger)tableView:(UITableView *)tableView
- numberOfRowsInSection:(NSInteger)section
-{
-    NSArray * events = [self eventsForDate:self.calendarPicker.highlightedDate];
-    return [events count];
-}
-
-- (UITableViewCell *)tableView:(UITableView *)tableView
-         cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:@"CELL_EVENT"];
-    if (cell == nil)
-        cell = [[UITableViewCell alloc] initWithStyle:(UITableViewCellStyleSubtitle) reuseIdentifier:@"CELL_EVENT"];
-    
-    NSArray * events = [self eventsForDate:self.calendarPicker.highlightedDate];
-    EKEvent * event = [events objectAtIndex:indexPath.row];
-    
-    cell.textLabel.text = event.title;
-    cell.detailTextLabel.text = event.notes;
-    
-    return cell;
+    NSLog(@"%@",date);
 }
 
 #pragma mark -
@@ -171,15 +92,11 @@
 {
     [super viewDidLoad];
     
-    self.eventsTable.delegate = self;
-    self.eventsTable.dataSource = self;
-    
     self.calendarPicker.delegate = self;
     self.calendarPicker.dataSource = self;
-    [self.view addSubview:self.calendarShadow];
-    [self calendarPicker:self.calendarPicker animateNewHeight:self.calendarPicker.bounds.size.height];
-
-    [self configTapped:nil];
+    [self.calendarPicker setDaysProvider:[[ABCalendarPickerDefaultTeacherKitDaysProvider alloc]init]];
+    [self.calendarPicker setWeekdaysProvider:[[ABCalendarPickerDefaultTeacherKitWeekDaysProvider alloc] init]];
+    self.calendarPicker.calendar.firstWeekday = 1;
 }
 
 - (void)didReceiveMemoryWarning
@@ -188,10 +105,4 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (void)viewDidUnload {
-    [self setCalendarPicker:nil];
-    [self setEventsTable:nil];
-    [self setConfigPanel:nil];
-    [super viewDidUnload];
-}
 @end
